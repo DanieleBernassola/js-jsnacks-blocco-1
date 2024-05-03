8c815045547143f64e761c7219558d616657f009
DanieleBernassola <danielebernassola02@gmail.com>
2024-05-03 13:09:44 +0200

    [UPD] Various fixes in js

--- jsnack3/js/main.js
+++ jsnack3/js/main.js
@@ -4,10 +4,10 @@ let userNumbers = [];
 let somma = 0;
 
 for (let i = 0; i < 10; i++){
-    let userNumber = parseInt(prompt('Inserisci un numero'));
+    let userNumber = parseInt(prompt(`Inserisci numero ${i + 1}`));
     console.log('Il numero inserito è: ', userNumber);
     userNumbers.push(userNumber);
-    somma = somma + userNumbers[i];
+    somma += userNumbers[i];
 }
 
 console.log('La somma è: ', somma);
