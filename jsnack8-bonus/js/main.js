07b5da6e5fe362bed6193cfbfca2bfdf4b5f0f5c
DanieleBernassola <danielebernassola02@gmail.com>
2024-05-03 17:08:06 +0200

    [FIX] Added comments

--- jsnack8-bonus/js/main.js
+++ jsnack8-bonus/js/main.js
@@ -4,10 +4,14 @@ let numberDigits = [];
 let somma = 0;
 
 for (let i = 1; i <= 4; i++){
+    // Inserimento cifra
     let numberDigit = prompt(`Inserisci la ${i}a cifra`);
+    // Verifica se è una sola cifra
     if (numberDigit.length === 1){
+        // Converte in numero
         numberDigit = parseInt(numberDigit);
         console.log('Hai inserito la seguente cifra:', numberDigit);
+        // Verifica se è numero
         if (!isNaN(numberDigit)){
             numberDigits.push(numberDigit);
             somma += numberDigit;
@@ -23,7 +27,3 @@ for (let i = 1; i <= 4; i++){
 
 console.log('Questo è il numero che hai inserito:', numberDigits);
 console.log('Questo è il risultato della somma delle cifre:', somma);
-
-// let numberDigit = prompt();
-// console.log(numberDigit.length);
-// numberDigit = parseInt(numberDigit);
