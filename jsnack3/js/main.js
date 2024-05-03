576fbe85714e5e2b6571a873799e4e83f6abe6f7
DanieleBernassola <danielebernassola02@gmail.com>
2024-05-03 14:53:31 +0200

    [UPD] Added comments in js

--- jsnack3/js/main.js
+++ jsnack3/js/main.js
@@ -1,12 +1,17 @@
 'use strict';
 
+// creo array per numeri(opzionale)
 let userNumbers = [];
+// inizializzo somma
 let somma = 0;
 
 for (let i = 0; i < 10; i++){
+    // inserisco numero
     let userNumber = parseInt(prompt(`Inserisci numero ${i + 1}`));
     console.log('Il numero inserito è: ', userNumber);
+    // pusho numero nell'array
     userNumbers.push(userNumber);
+    // aggiungo alla somma
     somma += userNumbers[i];
 }
 
