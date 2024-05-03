fc93a0b2979d57b81b5dc647f9cc8e87653f431b
DanieleBernassola <danielebernassola02@gmail.com>
2024-05-03 15:19:01 +0200

    [UPD] Added solution for snack5

--- jsnack5/js/main.js
+++ jsnack5/js/main.js
@@ -1,7 +1,22 @@
 'use strict';
 
-// let oddNumbers = [];
+// array numeri dispari vuoto
+let oddNumbers = [];
 
-// for(let i = 1; i <= 6; i++){
-//     let numero = parseInt(prompt(`Inserisci il numero ${i}`));
-// }
+for(let i = 1; i <= 6; i++){
+    // inserimento numeri
+    let number = parseInt(prompt(`Inserisci il ${i}o numero`));
+    // verifica se è un numero
+    if (!isNaN(number)){
+        console.log('Hai inserito questo numero:', number);
+        if (number % 2 === 1) {
+            // se il numero è dispari lo aggiunge all'array
+            oddNumbers.push(number);
+        } 
+    } else {
+        console.log('Non hai inserito un valore numerico');
+        i--;
+    }
+}
+
+console.log('Questo è il contenitore dei numeri dispari:', oddNumbers);
